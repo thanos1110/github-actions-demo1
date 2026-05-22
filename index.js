f1b269b242339452b4cf3f2c9bbfdc091795c3ba
thanos1110 <nirnjanpatil5434@gmail.com>
2026-05-22 22:32:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World!' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
